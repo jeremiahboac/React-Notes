16131415a1c0c37c3cd10de6a1508e56613bc7ae
Jeremiah Boac <jeremiahboac@gmail.com>
2022-08-23 13:50:22 +0800

    Update Search.js

--- react-notes/src/components/Search.js
+++ react-notes/src/components/Search.js
@@ -1,12 +1,17 @@
 import { MdSearch } from "react-icons/md"
 
 const Search = ({handleSearchNote}) => {
+  const search = (event) => {
+    const searchText = event.target.value
+    handleSearchNote(searchText)
+  }
+
     return (
         <div className="search">
           <MdSearch className="search-icon" size="1.3em" />   
           <input type="text" 
           placeholder="Type to search..." 
-          onChange={(event) => handleSearchNote(event.target.value)}/>
+          onChange={search}/>
         </div>
     )
 }
